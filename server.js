4c3d06642bc6fa887c5d421c28208d4f40a29054
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2018-03-20 00:24:36 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParse= require('body-parser');
+const _ = require('lodash');
+
+const {mongoose} = require('./db/mongoose');
+
+const{ObjectID}= require('mongodb');
+const time = new Date().getTime();
+
+const port =  process.env.PORT;
+
+
+
+
+var app = express();
+app.use(bodyParse.json());
+
+
+app.listen(port, ()=>{
+    console.log(`server is up on ${port}`);
+});
+
+
+module.exports= {app};
+
+
