363f1d9e9006ae4ec1b016dd2c8a9fa03e89b893
Rishabh Budhiraja <rishabh.budhiraja@gmail.com>
2018-03-20 01:28:52 +0530

    added heroku app support

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParse= require('body-parser');
 const _ = require('lodash');
-
-const {mongoose} = require('./db/mongoose');
+const hbs = require('handlebars');
+var app = express();
+//const {mongoose} = require('./db/mongoose');
 
 const{ObjectID}= require('mongodb');
 const time = new Date().getTime();
@@ -10,10 +11,35 @@ const time = new Date().getTime();
 const port =  3000;
 
 
+app.get('/',(req,res) => {
+
+    res.render('home.hbs',{
+    pageTitle : "Home page",
+});
+});
 
 
-var app = express();
-app.use(bodyParse.json());
+
+app.get('/why',(req,res) => {
+
+    res.render('why.hbs',{
+    pageTitle : "Home page",
+});
+});
+
+app.get('/sms',(req,res) => {
+
+    res.render('sms.hbs',{
+    pageTitle : "Send Money Securely",
+});
+});
+
+app.get('/contact',(req,res) => {
+
+    res.render('contact.hbs',{
+    pageTitle : "Contact Page",
+});
+});
 
 
 app.listen(port, ()=>{
